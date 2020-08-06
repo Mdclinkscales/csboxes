13b20839c119a0009ff8bd84d758f8b2fa803351
Michael Clinkscales <mdclinkscales@gmail.com>
2020-08-06 10:54:27 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace boxes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<object> Box = new List<object>();
+            Box.Add(7);
+            Box.Add(28);
+            Box.Add(-1);
+            Box.Add(true);
+            Box.Add("chair");
+            int sum = 0;
+            for(int i = 0; i<Box.Count; i++){
+                Console.WriteLine(Box[i]);
+                if (Box[i] is int){
+                    sum = sum + (int)Box[i];
+                }
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
